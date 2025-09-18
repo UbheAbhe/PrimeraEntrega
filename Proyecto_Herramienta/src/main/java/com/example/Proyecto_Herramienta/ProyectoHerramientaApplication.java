b94cffd92e926d627811aca757ef0281f6bf242a
Eds0n08 <galindosuazoedson@gmail.com>
2025-09-18 17:46:44 -0500

    Add files via upload

---
+++ Proyecto_Herramienta/src/main/java/com/example/Proyecto_Herramienta/ProyectoHerramientaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Herramienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHerramientaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHerramientaApplication.class, args);
+	}
+
+}
